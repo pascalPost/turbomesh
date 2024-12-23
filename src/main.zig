32e2bab5fb6b44df3c367184ebee04867b5092fa
Pascal Post <pascal-post@mailbox.org>
2024-12-23 23:23:48 +0100

    WIP: fixing build

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const types = @import("types.zig");
 const cgns = @import("cgns.zig");
+const spline = @import("spline.zig");
 
 const Mat2d = types.Mat2d;
 const Index2d = types.Index2d;
@@ -9,42 +10,42 @@ const Float = types.Float;
 const Index = types.Index;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(gpa.deinit() == .ok);
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer std.debug.assert(gpa.deinit() == .ok);
+    // const allocator = gpa.allocator();
 
-    var buffer: [21 * 17]Float = undefined;
+    // var buffer: [21 * 17]Float = undefined;
 
-    const block_names = [_][]const u8{
-        "block_0",
-    };
+    // const block_names = [_][]const u8{
+    //     "block_0",
+    // };
 
-    var block_points = [_]Mat2d{
-        try Mat2d.init(allocator, .{ 21, 17 }),
-    };
+    // var block_points = [_]Mat2d{
+    //     try Mat2d.init(allocator, .{ 21, 17 }),
+    // };
 
-    defer {
-        for (block_points) |block| {
-            block.deinit(allocator);
-        }
-    }
+    // defer {
+    //     for (block_points) |block| {
+    //         block.deinit(allocator);
+    //     }
+    // }
 
-    for (block_points) |block| {
-        const size = block.size;
+    // for (block_points) |block| {
+    //     const size = block.size;
 
-        var idx: usize = 0;
-        var i: usize = 0;
-        while (i < size[0]) : (i += 1) {
-            var j: usize = 0;
-            while (j < size[1]) : (j += 1) {
-                block.data[idx] = Vec2d{ @floatFromInt(i), @floatFromInt(j) };
-                idx += 1;
-            }
-        }
-    }
+    //     var idx: usize = 0;
+    //     var i: usize = 0;
+    //     while (i < size[0]) : (i += 1) {
+    //         var j: usize = 0;
+    //         while (j < size[1]) : (j += 1) {
+    //             block.data[idx] = Vec2d{ @floatFromInt(i), @floatFromInt(j) };
+    //             idx += 1;
+    //         }
+    //     }
+    // }
 
-    const filename = "example.cgns";
-    try cgns.write(filename, &block_names, &block_points, &buffer);
+    // const filename = "example.cgns";
+    // try cgns.write(filename, &block_names, &block_points, &buffer);
 }
 
 test "simple test" {
